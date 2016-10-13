e9393787cfd9d10eeb8526b043350b354ad3b4cc
Ashok <ashokicreate@gmail.com>
2016-10-13 13:09:02 +0530

    Added Slider Button

---
+++ oneMoreSample/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  oneMoreSample
+//
+//  Created by Ashok on 10/12/16.
+//  Copyright © 2016 Ashok. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: NSObject {
+
+}
